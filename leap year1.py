97148011e331f65472ca9da52b70b77f0f35a4de
Sajith359 <83387345+Sajith359@users.noreply.github.com>
2021-06-04 10:36:59 +0530

    Add files via upload

---
+++ leap year1.py
@@ -0,0 +1,17 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> year = 2000
+>>> if (year % 4) == 0:
+   if (year % 100) == 0:
+       if (year % 400) == 0:
+           print("{0} is a leap year".format(year))
+       else:
+           print("{0} is not a leap year".format(year))
+   else:
+       print("{0} is a leap year".format(year))
+else:
+   print("{0} is not a leap year".format(year))
+
+   
+2000 is a leap year
+>>> 
