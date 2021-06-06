6e8dbf865d67e723620edb6cbecf18c2bde46c91
Sajith359 <83387345+Sajith359@users.noreply.github.com>
2021-06-06 09:35:43 -0700

    Update roll_dice.py
    Improved spacing between variables.

--- roll_dice.py
+++ roll_dice.py
@@ -1,9 +1,9 @@
 # The goal of this script is to check whether two rolls of a dice either sum upto 7 or 11.
 import random
 
-min=1
-max=6
-roll_again="yes"
+min = 1
+max = 6
+roll_again = "yes"
 die1 = random.randint(1,6)
 die2 = random.randint(1,6)
 print("The dice rolled: {}, {}".format(die1, die2))
@@ -12,4 +12,3 @@ if (die1 + die2) == 7 or (die1 + die2)== 11:
 	print("You win!")
 else:
     print("you lost")
-  
