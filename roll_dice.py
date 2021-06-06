9aab95def91b1584ec7de1eb726e6f727dc29839
Sajith359 <83387345+Sajith359@users.noreply.github.com>
2021-06-06 09:35:06 -0700

    Update and rename pyhton  dice.py to roll_dice.py
    Made this run from the command line instead of the interpreter.

---
+++ roll_dice.py
@@ -0,0 +1,15 @@
+# The goal of this script is to check whether two rolls of a dice either sum upto 7 or 11.
+import random
+
+min=1
+max=6
+roll_again="yes"
+die1 = random.randint(1,6)
+die2 = random.randint(1,6)
+print("The dice rolled: {}, {}".format(die1, die2))
+print("The sum of dice rolled values is {}".format(die1 + die2))
+if (die1 + die2) == 7 or (die1 + die2)== 11:
+	print("You win!")
+else:
+    print("you lost")
+  
